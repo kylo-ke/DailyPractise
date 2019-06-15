068b4e579ee34bfa82c4e90b50072c880b6610df
2513084541 <2513084541@qq.com>
2019-06-15 11:25:15 +0800

    modified the dijkstraAlgorithem

--- DailyPractise.cpp
+++ DailyPractise.cpp
@@ -11,7 +11,7 @@ int main()
 {
 	Graph    djGarph(3);
 	
-	djGarph.insert(1, ArcEdge(2, 1));
+	djGarph.insert(1, ArcEdge(2, 6));
 	djGarph.insert(1, ArcEdge(6, 7));
 	djGarph.insert(1, ArcEdge(7, 4));
 	djGarph.insert(2, ArcEdge(3, 5));
@@ -22,12 +22,43 @@ int main()
 	djGarph.insert(5, ArcEdge(6, 9));
 	djGarph.insert(6, ArcEdge(7, 3));
 
-	vector<int>   arrs(10, 4);
-	arrs.resize(20);
-	for (auto& n : arrs)
-		cout << n << endl;
-
-	cout << djGarph.dijkstra(1, 4);
+	cout << djGarph.dijkstra(6, 4);
 
 	return 0;
 }
+
+
+//int dijkstra(const int srcNode, const int dstNode) const
+//{
+//	vector<int>   distance(_nodeSize, INT_MAX);
+//	vector<bool>	exist(_nodeSize, false);
+//	distance[srcNode] = 0;
+//	exist[srcNode] = true;
+//	int head = srcNode;
+//	while (exist[dstNode] != true)
+//	{
+//		auto  fistArc = _GraphData[head];
+//		for (auto ptr = fistArc.begin(); ptr != fistArc.end(); ptr++)
+//		{
+//			int ptrNodeNum = ptr->number;
+//			if (exist[ptrNodeNum] != true && distance[ptrNodeNum] > (distance[head] + ptr->weight))
+//			{
+//				distance[ptrNodeNum] = (distance[head] + ptr->weight);
+//			}
+//		}
+//		//find the minest number in distance
+//		int minPostion = 1;
+//		int compareNum = INT_MAX;
+//		for (int index = 1; index < distance.size(); index++)
+//		{
+//			if (exist[index] != true && index != head && distance[index] < compareNum)
+//			{
+//				minPostion = index;
+//				compareNum = distance[index];
+//			}
+//		}
+//		exist[minPostion] = true;
+//		head = minPostion;
+//	}
+//	return distance[dstNode];
+//}
--- dijkstra.h
+++ dijkstra.h
@@ -14,22 +14,19 @@ public:
 	int number;
 	int weight;
 	//ArcEdge* next;
-
 	bool operator==(const ArcEdge& rhs) const
 	{
 		if (this->number == rhs.number)
-		{
 			return true;
-		}
 		else
-			false;
+			return	false;
 	}
 	ArcEdge(const int num, const int wei) :number(num), weight(wei) {};
 };
 
 struct Node
 {
-	//int			number;
+	//int	 number;
 	ArcEdge* firstArc;
 };
 
@@ -43,15 +40,22 @@ public:
 	//double 
 	bool insert(const int vertexNode, const ArcEdge& insertEdge )
 	{
-		if (_nodeSize <= vertexNode  )
+		int resizeNum = 0;
+		if (vertexNode < insertEdge.number)
+			resizeNum = insertEdge.number;
+		else
+			resizeNum = resizeNum;
+
+
+
+		if (_nodeSize <= resizeNum)
 		{
-			_GraphData.resize(int(vertexNode*1.5));
-			_nodeSize = int(vertexNode*1.5);
-			cout << "use ";
+			_GraphData.resize(int(resizeNum*1.5));
+			_nodeSize = int(resizeNum*1.5);
 		}
+
 		auto&  fistArcEdge = _GraphData[vertexNode];
 		auto ptr = std::find(fistArcEdge.begin(), fistArcEdge.end(), insertEdge);
-		auto itr = fistArcEdge.end();
 		if (ptr == fistArcEdge.end())
 		{
 			fistArcEdge.push_back(insertEdge);
@@ -60,18 +64,14 @@ public:
 		{
 			return false;
 		}
+
 		//double direction edge
-		if (_nodeSize <= insertEdge.number)
-		{
-			_GraphData.resize(int (insertEdge.number*1.5));
-			_nodeSize = int(insertEdge.number*1.5);
-		}
-		fistArcEdge = _GraphData[insertEdge.number];
+		auto& otherfistArcEdge = _GraphData[insertEdge.number];
 		ArcEdge   tempVal(vertexNode, insertEdge.weight);
-		ptr = std::find(fistArcEdge.begin(), fistArcEdge.end(), tempVal);
-		if (ptr == fistArcEdge.end())
+		auto itr = std::find(otherfistArcEdge.begin(), otherfistArcEdge.end(), tempVal);
+		if (itr == otherfistArcEdge.end())
 		{
-			fistArcEdge.push_back(tempVal);
+			otherfistArcEdge.push_back(tempVal);
 		}
 		else
 		{
@@ -102,14 +102,10 @@ public:
 			}
 			//find the minest number in distance
 			int minPostion = 1;
-			//if (srcNode == minPostion)
-			//{
-			//	minPostion = 2;
-			//}
 			int compareNum = INT_MAX;
 			for (int index = 1; index < distance.size(); index++)
 			{
-				if (index != head && distance[index] < compareNum)
+				if ( exist[index] != true && index != head && distance[index] < compareNum)
 				{
 					minPostion = index;
 					compareNum = distance[index];
